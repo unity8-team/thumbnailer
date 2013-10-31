7dbc3b97e90f1de0e5e851f04668a91c716fd664
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-10-31 15:52:49 +0200

    Started downloader code.

---
+++ tools/lastfmtest.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ */
+
+#include<string>
+#include<libsoup/soup.h>
+
+using namespace std;
+
+void getImage() {
+    string artist = "The Prodigy";
+    string album = "Music for the Jilted Generation";
+    SoupSession *s = soup_session_sync_new();
+    g_object_unref(s);
+}
+
+int main(int /*argc*/, char **/*argv*/) {
+    getImage();
+    return 0;
+}
