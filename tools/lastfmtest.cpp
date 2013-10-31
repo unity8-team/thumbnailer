0d6d83b53e2129f1fedd3a2b9281320d998c841c
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-10-31 16:04:17 +0200

    Pull data from lastfm.

--- tools/lastfmtest.cpp
+++ tools/lastfmtest.cpp
@@ -17,14 +17,28 @@
  */
 
 #include<string>
+#include<cstdio>
 #include<libsoup/soup.h>
 
 using namespace std;
 
 void getImage() {
+    const char *lastfmTemplate = "http://ws.audioscrobbler.com/1.0/album/%s/%s/info.xml";
     string artist = "The Prodigy";
     string album = "Music for the Jilted Generation";
+    const int bufsize = 1024;
+    char buf[bufsize];
+    snprintf(buf, bufsize, lastfmTemplate, artist.c_str(), album.c_str());
     SoupSession *s = soup_session_sync_new();
+    SoupMessage *msg = soup_message_new("GET", buf);
+    guint status;
+    status = soup_session_send_message(s, msg);
+    if(!SOUP_STATUS_IS_SUCCESSFUL(status)) {
+        fprintf(stderr, "Fail\n");
+        return;
+    }
+    printf("%s\n", msg->response_body->data);
+    g_object_unref(msg);
     g_object_unref(s);
 }
 
