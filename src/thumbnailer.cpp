107e0e1ad5e3b2a1b19d9ffc793f117ac6515d55
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-11-08 16:39:31 +0200

    Reorganised generator code.

--- src/thumbnailer.cpp
+++ src/thumbnailer.cpp
@@ -26,12 +26,19 @@
 #include<gst/gst.h>
 #include<stdexcept>
 #include<random>
+#include<gio/gio.h>
+#include<glib.h>
+#include<memory>
 
 using namespace std;
 
 class ThumbnailerPrivate {
 private:
     mt19937 rnd;
+
+    string create_audio_thumbnail(const string &abspath, ThumbnailSize desired_size);
+    string create_video_thumbnail(const string &abspath, ThumbnailSize desired_size);
+    string create_generic_thumbnail(const string &abspath, ThumbnailSize desired_size);
 
 public:
     ThumbnailCache cache;
@@ -44,31 +51,7 @@ public:
     string create_thumbnail(const string &abspath, ThumbnailSize desired_size);
 };
 
-string ThumbnailerPrivate::create_thumbnail(const string &abspath, ThumbnailSize desired_size) {
-    int tmpw, tmph;
-    // Every now and then see if we have too much stuff and delete them if so.
-    if((rnd() % 100) == 0) { // No, this is not perfectly random. It does not need to be.
-        cache.prune();
-    }
-    // We do not know what the file pointed to is. File suffixes may be missing
-    // or just plain lie. So we try to open it one by one with the handlers we have.
-    // Go from least resource intensive to most.
-    string tnfile = cache.get_cache_file_name(abspath, desired_size);
-    char filebuf[] = "/tmp/some/long/text/here/so/path/will/fit";
-    string tmpname = tmpnam(filebuf);
-
-    // Special case: full size image files are their own preview.
-    if(desired_size == TN_SIZE_ORIGINAL &&
-            gdk_pixbuf_get_file_info(abspath.c_str(), &tmpw, &tmph)) {
-        return abspath;
-    }
-    try {
-        if(scaler.scale(abspath, tnfile, desired_size, abspath))
-            return tnfile;
-    } catch(runtime_error &e) {
-        // Fail is ok, just try the next one.
-    }
-    bool extracted = false;
+string ThumbnailerPrivate::create_audio_thumbnail(const string &abspath, ThumbnailSize desired_size) {
     // There was a symbol clas between 1.0 and 0.10 versions of
     // GStreamer on the desktop so we need to disable in-process
     // usage of gstreamer. Re-enable this once desktop moves to
@@ -86,19 +69,69 @@ string ThumbnailerPrivate::create_thumbnail(const string &abspath, ThumbnailSize
         return tnfile;
     }
     */
+    return "";
+}
+string ThumbnailerPrivate::create_generic_thumbnail(const string &abspath, ThumbnailSize desired_size) {
+    int tmpw, tmph;
+    string tnfile = cache.get_cache_file_name(abspath, desired_size);
+    // Special case: full size image files are their own preview.
+    if(desired_size == TN_SIZE_ORIGINAL &&
+       gdk_pixbuf_get_file_info(abspath.c_str(), &tmpw, &tmph)) {
+        return abspath;
+    }
     try {
-        if(video.extract(abspath, tmpname)) {
-            extracted = true;
-        }
+        if(scaler.scale(abspath, tnfile, desired_size, abspath))
+            return tnfile;
     } catch(runtime_error &e) {
     }
-    if(extracted) {
-        scaler.scale(tmpname, tnfile, desired_size, abspath); // If this throws, let it propagate.
+    return "";
+}
+
+string ThumbnailerPrivate::create_video_thumbnail(const string &abspath, ThumbnailSize desired_size) {
+    char filebuf[] = "/tmp/some/long/text/here/so/path/will/fit";
+    string tnfile = cache.get_cache_file_name(abspath, desired_size);
+    string tmpname = tmpnam(filebuf);
+    if(video.extract(abspath, tmpname)) {
+        scaler.scale(tmpname, tnfile, desired_size, abspath);
         unlink(tmpname.c_str());
         return tnfile;
     }
-    // None of our handlers knew how to handle it.
-    return "";
+    throw runtime_error("Video extraction failed.");
+}
+
+string ThumbnailerPrivate::create_thumbnail(const string &abspath, ThumbnailSize desired_size) {
+    // Every now and then see if we have too much stuff and delete them if so.
+    if((rnd() % 100) == 0) { // No, this is not perfectly random. It does not need to be.
+        cache.prune();
+    }
+    std::unique_ptr<GFile, void(*)(void *)> file(
+            g_file_new_for_path(abspath.c_str()), g_object_unref);
+    if(!file) {
+        return "";
+    }
+
+    std::unique_ptr<GFileInfo, void(*)(void *)> info(
+            g_file_query_info(file.get(), G_FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE,
+                G_FILE_QUERY_INFO_NONE, /* cancellable */ NULL, /* error */NULL),
+            g_object_unref);
+    if(!info) {
+        return "";
+    }
+
+    std::string content_type(g_file_info_get_content_type(info.get()));
+    if (content_type.empty()) {
+        return "";
+    }
+
+    if (content_type.find("audio/") == 0) {
+        return create_audio_thumbnail(abspath, desired_size);
+    }
+
+    if (content_type.find("video/") == 0) {
+        return create_video_thumbnail(abspath, desired_size);
+    }
+
+    return create_generic_thumbnail(abspath, desired_size);
 }
 
 Thumbnailer::Thumbnailer() {
