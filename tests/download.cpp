0a276652992ee5e8ecbc99798bbdf00fab05f6da
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2014-03-19 14:02:51 +0200

    Created fake server and added a test that uses it.

--- tests/download.cpp
+++ tests/download.cpp
@@ -18,8 +18,43 @@
 
 #include<gtest/gtest.h>
 #include<internal/lastfmdownloader.h>
+#include<cstdio>
+
+using namespace std;
+
+const char *testimage = "abc";
+
+class FakeDownloader : public HttpDownloader {
+private:
+    constexpr static const char *imloc = "http://dummy";
+    constexpr static const char *xml = "<album><coverart><large>http://dummy</large></coverart></album>";
+
+public:
+    std::string download(const std::string &url) override {
+        if(url.find("audioscrobbler") != std::string::npos) {
+            return xml;
+        }
+        if(url == imloc) {
+            return testimage;
+        }
+        throw std::runtime_error("Tried to get unknown data from FakeDownloader.");
+    }
+};
 
 TEST(Downloader, canned) {
+    LastFMDownloader lfdl(new FakeDownloader());
+    string artist("Some guy");
+    string album("Some album");
+    string outfile("/tmp/temptestfile");
+    ASSERT_TRUE(lfdl.download(artist, album, outfile));
+    char output[4];
+    FILE *f = fopen(outfile.c_str(), "r");
+    fread(output, 3, 1, f);
+    output[3] = '\0';
+    fclose(f);
+    string s1(testimage);
+    string s2(output);
+    ASSERT_EQ(s1, s2);
 }
 
 
