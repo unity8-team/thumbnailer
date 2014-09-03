5f3b25ced484ee0571c57721e7594fdb08c77a68
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2014-09-03 11:09:29 +0300

    Move embedded thumbnail extraction out of process to a helper binary.

--- src/audioimageextractor.cpp
+++ src/audioimageextractor.cpp
@@ -21,39 +21,30 @@
 #include<unistd.h>
 #include<stdexcept>
 #include<memory>
-#include <gst/pbutils/pbutils.h>
+#include"internal/videoscreenshotter.h"
 
 using namespace std;
 
-class GstInitializer {
-public:
-    GstInitializer() { gst_init(nullptr, nullptr); };
-};
-
 class AudioImageExtractorPrivate {
+
+public:
+
+    bool extract(const string &ifname, const string &ofname);
+
+private:
+    // Use the external binary to extract the embedded image.
+    // We could use the vs object in the Thumbnailer class and remove
+    // AudioImageExtractor completely but it is possible that
+    // this is refactored to use a dbus service for efficiency
+    // so having this class around will make that refactoring easier.
+    VideoScreenshotter vs;
 };
 
-static bool has_tag(const GstTagList *tlist, const gchar *tagname) {
-  const GValue *val = gst_tag_list_get_value_index(tlist, tagname, 0);
-  return val;
-}
-
-static void extract_image(const GstTagList *tlist, FILE *outf) {
-  const GValue *val = gst_tag_list_get_value_index(tlist, GST_TAG_IMAGE, 0);
-  GstSample *sample = (GstSample*)g_value_get_boxed(val);
-  GstBuffer *buf = gst_sample_get_buffer(sample);
-
-  for(guint i=0; i<gst_buffer_n_memory(buf); i++) {
-    GstMemory *mem = gst_buffer_peek_memory(buf, i);
-    GstMapInfo mi;
-    gst_memory_map(mem, &mi, GST_MAP_READ);
-    fwrite(mi.data, 1, mi.size, outf);
-    gst_memory_unmap(mem, &mi);
-  }
+bool AudioImageExtractorPrivate::extract(const string &ifname, const string &ofname) {
+    return vs.extract(ifname, ofname);
 }
 
 AudioImageExtractor::AudioImageExtractor() {
-    static GstInitializer i; // C++ standard guarantees this to be lazy and thread safe.
     p = new AudioImageExtractorPrivate();
 }
 
@@ -63,38 +54,5 @@ AudioImageExtractor::~AudioImageExtractor() {
 
 
 bool AudioImageExtractor::extract(const string &ifname, const string &ofname) {
-    string uri("file://");
-    if(ifname[0] != '/') {
-        uri += getcwd(nullptr, 0);
-        uri += '/';
-    }
-    uri += ifname;
-    GError *err = nullptr;
-    unique_ptr<GstDiscoverer, void(*)(GstDiscoverer*)> dsc(
-            gst_discoverer_new(GST_SECOND, &err),
-            [](GstDiscoverer *t) {g_object_unref(G_OBJECT(t));});
-    if(err) {
-        string msg(err->message);
-        g_error_free(err);
-        throw runtime_error(msg);
-    }
-    unique_ptr<GstDiscovererInfo, void(*)(GstDiscovererInfo*)> info(
-            gst_discoverer_discover_uri(dsc.get(), uri.c_str(), &err),
-            [](GstDiscovererInfo *t) {g_object_unref(G_OBJECT(t));});
-    if(err) {
-        string msg(err->message);
-        g_error_free(err);
-        throw runtime_error(msg);
-    }
-    const GstTagList *tlist = gst_discoverer_info_get_tags(info.get());
-    if(!tlist) {
-        return false;
-    }
-    if(!has_tag(tlist, GST_TAG_IMAGE)) {
-        return false;
-    }
-    FILE *outfile = fopen(ofname.c_str(), "wb");
-    extract_image(tlist, outfile);
-    fclose(outfile);
-    return true;
+    return p->extract(ifname, ofname);
 }
