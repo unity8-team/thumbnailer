239df40832932545ed092e171324beaf5bbc0fce
Pawel Stolowski <pawel.stolowski@canonical.com>
2014-09-24 18:25:31 +0200

    Create thumbnailer-common package with album_missing.png icon.

--- plugins/Ubuntu/Thumbnailer/albumartgenerator.cpp
+++ plugins/Ubuntu/Thumbnailer/albumartgenerator.cpp
@@ -26,7 +26,7 @@
 #include <QDBusUnixFileDescriptor>
 #include <QDBusReply>
 
-static const char DEFAULT_ALBUM_ART[] = "/usr/share/unity/icons/album_missing.png";
+static const char DEFAULT_ALBUM_ART[] = "/usr/share/thumbnailer/icons/album_missing.png";
 
 static const char BUS_NAME[] = "com.canonical.Thumbnailer";
 static const char BUS_PATH[] = "/com/canonical/Thumbnailer";
