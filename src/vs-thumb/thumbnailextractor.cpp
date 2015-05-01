2197e8ec014f128e19c3a2bcbbd637dd6a241328
Michi Henning <michi.henning@canonical.com>
2015-05-01 10:34:15 +0100

    Suppressed more warnings from gstreamer that show up only on Arm.

--- src/vs-thumb/thumbnailextractor.cpp
+++ src/vs-thumb/thumbnailextractor.cpp
@@ -25,6 +25,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 #pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wcast-align"
 
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <gst/gst.h>
