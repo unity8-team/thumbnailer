5906169d8b20a9dcbcf0e0508d910a4f47a76bde
James Henstridge <james@jamesh.id.au>
2015-05-14 16:21:51 +0800

    Add a test for gobj_ptr<T> copy behaviour, and remove GdkPixbuf dependency from this suite (just use GObject/GInitiallyUnowned).

--- tests/gobj_ptr.cpp
+++ tests/gobj_ptr.cpp
@@ -21,152 +21,164 @@
 #pragma GCC diagnostic ignored "-Wcast-qual"
 
 #include<internal/gobj_memory.h>
-#include<gdk-pixbuf/gdk-pixbuf.h>
+#include<glib-object.h>
 #include<gtest/gtest.h>
 
 using namespace std;
 
 TEST(Gobj_ptr, trivial) {
-    gobj_ptr<GdkPixbuf> basic(gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480));
-    ASSERT_TRUE(bool(basic));
-    ASSERT_TRUE(gdk_pixbuf_get_width(basic.get()) == 640);
-    ASSERT_TRUE(gdk_pixbuf_get_height(basic.get()) == 480);
+    gobj_ptr<GObject> basic(G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr)));
+    EXPECT_TRUE(bool(basic));
+    EXPECT_TRUE(G_IS_OBJECT(basic.get()));
 }
 
 TEST(Gobj_ptr, compare) {
-    GdkPixbuf *pb1 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    GdkPixbuf *pb2 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    if(pb1 > pb2) {
-        std::swap(pb1, pb2);
+    GObject *o1 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    GObject *o2 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    if(o1 > o2) {
+        std::swap(o1, o2);
     }
-    ASSERT_TRUE(pb1 < pb2);
-    gobj_ptr<GdkPixbuf> u1(pb1);
-    gobj_ptr<GdkPixbuf> u2(pb2);
+    ASSERT_TRUE(o1 < o2);
+    gobj_ptr<GObject> u1(o1);
+    gobj_ptr<GObject> u2(o2);
 
-    ASSERT_TRUE(!(u1 == nullptr));
-    ASSERT_TRUE(u1 != nullptr);
-    ASSERT_TRUE(u1 != u2);
-    ASSERT_TRUE(!(u1 == u2));
-    ASSERT_TRUE(u1 < u2);
-    ASSERT_TRUE(!(u2 < u1));
-    ASSERT_TRUE(!(u1 == u2));
-    ASSERT_TRUE(!(u2 == u1));
-    ASSERT_TRUE(u1 <= u2);
-    ASSERT_TRUE(!(u2 <= u1));
+    EXPECT_TRUE(!(u1 == nullptr));
+    EXPECT_TRUE(u1 != nullptr);
+    EXPECT_TRUE(u1 != u2);
+    EXPECT_TRUE(!(u1 == u2));
+    EXPECT_TRUE(u1 < u2);
+    EXPECT_TRUE(!(u2 < u1));
+    EXPECT_TRUE(!(u1 == u2));
+    EXPECT_TRUE(!(u2 == u1));
+    EXPECT_TRUE(u1 <= u2);
+    EXPECT_TRUE(!(u2 <= u1));
 }
 
 // This is its own thing due to need to avoid double release.
 
 TEST(Gobj_ptr, equality) {
-    GdkPixbuf *pb = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    gobj_ptr<GdkPixbuf> u1(pb);
-    g_object_ref(G_OBJECT(pb));
-    gobj_ptr<GdkPixbuf> u2(pb);
-    ASSERT_TRUE(u1 == u2);
-    ASSERT_TRUE(u2 == u1);
-    ASSERT_TRUE(!(u1 != u2));
-    ASSERT_TRUE(!(u2 != u1));
+    GObject *o = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    gobj_ptr<GObject> u1(o);
+    g_object_ref(o);
+    gobj_ptr<GObject> u2(o);
+    EXPECT_TRUE(u1 == u2);
+    EXPECT_TRUE(u2 == u1);
+    EXPECT_TRUE(!(u1 != u2));
+    EXPECT_TRUE(!(u2 != u1));
 }
 
 TEST(Gobj_ptr, release) {
-    GdkPixbuf *pb = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    gobj_ptr<GdkPixbuf> u(pb);
-    ASSERT_TRUE(u != nullptr);
-    ASSERT_TRUE(u.get() != nullptr);
-    ASSERT_TRUE(pb == u.release());
-    ASSERT_TRUE(!u);
-    ASSERT_TRUE(u.get() == nullptr);
-    g_object_unref(pb);
-}
-
-void sub_func(GdkPixbuf *pb) {
-    ASSERT_TRUE(G_OBJECT(pb)->ref_count == 2);
-    gobj_ptr<GdkPixbuf> u(pb);
-    ASSERT_TRUE(G_OBJECT(pb)->ref_count == 2);
-    // Now it dies and refcount is reduced.
+    GObject *o = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    gobj_ptr<GObject> u(o);
+    EXPECT_TRUE(u != nullptr);
+    EXPECT_TRUE(u.get() != nullptr);
+    EXPECT_TRUE(o == u.release());
+    EXPECT_TRUE(!u);
+    EXPECT_TRUE(u.get() == nullptr);
+    g_object_unref(o);
 }
 
 TEST(Gobj_ptr, refcount) {
-    GdkPixbuf *pb = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    ASSERT_TRUE(G_OBJECT(pb)->ref_count == 1);
-    g_object_ref(G_OBJECT(pb));
-    sub_func(pb);
-    ASSERT_TRUE(G_OBJECT(pb)->ref_count == 1);
-    g_object_unref(G_OBJECT(pb));
+    GObject *o = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    EXPECT_EQ(1, o->ref_count);
+    g_object_ref(o);
+
+    {
+        EXPECT_EQ(2, o->ref_count);
+        gobj_ptr<GObject> u(o);
+        EXPECT_EQ(2, o->ref_count);
+        // Now it dies and refcount is reduced.
+    }
+
+    EXPECT_EQ(1, o->ref_count);
+    g_object_unref(o);
+}
+
+TEST(Gobj_ptr, copy) {
+    GObject *o = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    gobj_ptr<GObject> u(o);
+    EXPECT_EQ(1, u->ref_count);
+    gobj_ptr<GObject> u2(u);
+    EXPECT_EQ(2, u->ref_count);
+    gobj_ptr<GObject> u3 = u2;
+    EXPECT_EQ(3, u->ref_count);
+    u3.reset();
+    u2.reset();
+    EXPECT_EQ(1, u->ref_count);
 }
 
 TEST(Gobj_ptr, swap) {
-    GdkPixbuf *pb1 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    GdkPixbuf *pb2 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    gobj_ptr<GdkPixbuf> u1(pb1);
-    gobj_ptr<GdkPixbuf> u2(pb2);
+    GObject *o1 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    GObject *o2 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    gobj_ptr<GObject> u1(o1);
+    gobj_ptr<GObject> u2(o2);
 
     u1.swap(u2);
-    ASSERT_TRUE(u1.get() == pb2);
-    ASSERT_TRUE(u2.get() == pb1);
+    EXPECT_EQ(o2, u1.get());
+    EXPECT_EQ(o1, u2.get());
 
     std::swap(u1, u2);
-    ASSERT_TRUE(u1.get() == pb1);
-    ASSERT_TRUE(u2.get() == pb2);
+    EXPECT_EQ(o1, u1.get());
+    EXPECT_EQ(o2, u2.get());
 }
 
 TEST(Gobj_ptr, floating) {
-    GdkPixbuf *pb = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    bool got_exception = false;
-    g_object_force_floating(G_OBJECT(pb));
+    GObject *o = G_OBJECT(g_object_new(G_TYPE_INITIALLY_UNOWNED, nullptr));
     try {
-        gobj_ptr<GdkPixbuf> u(pb);
+        gobj_ptr<GObject> u(o);
+        FAIL();
     } catch(const invalid_argument &c) {
-        got_exception = true;
+        EXPECT_EQ("Tried to add a floating gobject into a gobj_ptr.", string(c.what()));
     }
-    g_object_unref(G_OBJECT(pb));
-    ASSERT_TRUE(got_exception);
+    // Object accepted after sinking.
+    g_object_ref_sink(o);
+    gobj_ptr<GObject> u(o);
 }
 
 TEST(Gobj_ptr, move) {
-    GdkPixbuf *pb1 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    GdkPixbuf *pb2 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    g_object_ref(G_OBJECT(pb1));
-    gobj_ptr<GdkPixbuf> u1(pb1);
-    gobj_ptr<GdkPixbuf> u2(pb2);
+    GObject *o1 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    GObject *o2 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    g_object_ref(o1);
+    gobj_ptr<GObject> u1(o1);
+    gobj_ptr<GObject> u2(o2);
     u1 = std::move(u2);
-    ASSERT_TRUE(u1.get() == pb2);
-    ASSERT_TRUE(!u2);
-    ASSERT_TRUE(G_OBJECT(pb1)->ref_count == 1);
-    g_object_unref(G_OBJECT(pb1));
+    EXPECT_TRUE(u1.get() == o2);
+    EXPECT_TRUE(!u2);
+    EXPECT_TRUE(o1->ref_count == 1);
+    g_object_unref(o1);
 }
 
 TEST(Gobj_ptr, null) {
-    GdkPixbuf *pb1 = NULL;
-    GdkPixbuf *pb3 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    gobj_ptr<GdkPixbuf> u1(pb1);
-    gobj_ptr<GdkPixbuf> u2(nullptr);
-    gobj_ptr<GdkPixbuf> u3(pb3);
+    GObject *o1 = NULL;
+    GObject *o3 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    gobj_ptr<GObject> u1(o1);
+    gobj_ptr<GObject> u2(nullptr);
+    gobj_ptr<GObject> u3(o3);
 
-    ASSERT_TRUE(!u1);
-    ASSERT_TRUE(!u2);
+    EXPECT_TRUE(!u1);
+    EXPECT_TRUE(!u2);
     u3 = nullptr;
-    ASSERT_TRUE(!u3);
+    EXPECT_TRUE(!u3);
 }
 
 TEST(Gobj_ptr, reset) {
-    GdkPixbuf *pb1 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    GdkPixbuf *pb2 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, 640, 480);
-    gobj_ptr<GdkPixbuf> u(pb1);
+    GObject *o1 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    GObject *o2 = G_OBJECT(g_object_new(G_TYPE_OBJECT, nullptr));
+    gobj_ptr<GObject> u(o1);
 
-    u.reset(pb2);
-    ASSERT_TRUE(u.get() == pb2);
+    u.reset(o2);
+    EXPECT_EQ(o2, u.get());
     u.reset(nullptr);
-    ASSERT_TRUE(!u);
+    EXPECT_TRUE(!u);
 }
 
 TEST(Gobj_ptr, sizeoftest) {
-    ASSERT_TRUE(sizeof(GdkPixbuf*) == sizeof(gobj_ptr<GdkPixbuf>));
+    EXPECT_EQ(sizeof(GObject*), sizeof(gobj_ptr<GObject>));
 }
 
 TEST(Gobj_ptr, deleter) {
-    gobj_ptr<GdkPixbuf> u1;
-    ASSERT_TRUE(u1.get_deleter() == g_object_unref);
+    gobj_ptr<GObject> u1;
+    EXPECT_EQ(g_object_unref, u1.get_deleter());
 }
 
 int main(int argc, char **argv) {
