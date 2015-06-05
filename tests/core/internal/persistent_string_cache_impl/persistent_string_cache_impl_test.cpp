285566395c21b488766d03a832b587f44af18e90
Michi Henning <michi.henning@canonical.com>
2015-06-05 17:08:20 +1000

    Re-instated compaction correctness test.

--- tests/core/internal/persistent_string_cache_impl/persistent_string_cache_impl_test.cpp
+++ tests/core/internal/persistent_string_cache_impl/persistent_string_cache_impl_test.cpp
@@ -1463,9 +1463,10 @@ TEST(PersistentStringCacheImpl, invalidate)
     EXPECT_EQ(0, c.size());
     EXPECT_EQ(0, c.size_in_bytes());
 
-    // For coverage.
+    // For coverage mainly, and to verify that compact() indeed compacts the DB.
     c.invalidate();
     c.compact();
+    EXPECT_LT(c.disk_size_in_bytes(), 1000);
 }
 
 TEST(PersistentStringCacheImpl, stats)
