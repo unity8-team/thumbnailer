ebada3879fe55136a7a0abe9bdcf46d14cb10244
Michi Henning <michi.henning@canonical.com>
2015-08-11 15:24:37 +1000

    Did a copy edit on the library doc. Removed graphviz from build deps and disabled inheritance graph generation for doc.

--- include/libthumbnailer-qt/thumbnailer-qt.h
+++ include/libthumbnailer-qt/thumbnailer-qt.h
@@ -38,12 +38,10 @@ class RequestImpl;
 
 /**
 \brief Holds a thumbnailer request.
-This object represents and holds all the information related to a thumbnailer request.
-When the user invokes any of the available methods in unity::thumbnailer::Thumbnailer he obtains
-a QSharedPointer to a unity::thumbnailer::Request, which sends a finished()signal upon its completion.
-After it finished the user can query the obtained image, if the request was successful or, in case of error,
-get the error string containing the failure report.
+
+This class stores the state of an in-progress or completed thumbnail request.
 */
+
 class Request : public QObject
 {
     Q_OBJECT
@@ -54,39 +52,42 @@ public:
     /// @endcond
 
     /**
-     \brief Returns if the request already finished.
-     At the instantiation of the request this property will be set to false.
-     \return true if the request finished, false otherwise
+    \brief Returns whether the request has completed.
+
+    \return `false` if the request is still in progress. Otherwise, the return
+    value is `true` (whether the request completed successfully or not).
     */
     bool isFinished() const;
 
     /**
-     \brief Returns the thumbnailer image.
-     \return A valid QImage if the request was successful, otherwise it returns an empty QImage.
+    \brief Returns the thumbnail.
+    \return A valid QImage if the request was successful and an empty `QImage`, otherwise.
     */
     QImage image() const;
 
     /**
-     \brief Returns the error message after the request finished.
-     \return The error string in case of failure, an empty QString otherwise
+    \brief Returns the error message for a failed request.
+    \return The error message in case of a failure and an empty `QString`, otherwise.
     */
     QString errorMessage() const;
 
     /**
-     \brief Returns if the request finished successfully.
-     \return true if the request finished successfully, false in case of any failure.
+    \brief Returns whether the request completed successfully.
+    \return `true` if the request completed successfully. Otherwise, if the request is still
+    in progress or has failed, the return value is `false`.
     */
     bool isValid() const;
 
     /**
-     \brief Blocks the calling thread until the request finishes.
-     This method is useful for those users who want to follow a synchronous behavior.
+    \brief Blocks the calling thread until the request completes.
+
+    \warning Calling this function from the main (GUI) thread might cause your user interface to freeze.
     */
     void waitForFinished();
 
 Q_SIGNALS:
     /**
-     \brief Emitted when the request finishes.
+    \brief This signal is emitted when the request completes.
     */
     void finished();
 
@@ -98,10 +99,33 @@ private:
 };
 
 /**
-\brief This class provides a way to generate and access thumbnails of video, audio and image files.
-After the user calls any of his public methods to obtain thumbnails it creates a QSharedPointer to a
-unity::thumbnailer::Request object which will hold the information and state of that particular request.
+\brief Class to obtain thumbnail images for various media types.
+
+Class Thumbnailer provides thumbnail images for local media (image, audio, and video) files, as well
+as album covers and artist images for many musicians and bands.
+
+Most common image formats, such as PNG, JPEG, BMP, and so on, are recognized. For streaming media, the
+recognized formats depend in the installed GStreamer codecs.
+
+For local media files, thumbnails are extracted directly from the file. (For audio files, this
+requires the file to contain embedded artwork.) For album covers and artist images,
+artwork is downloaded from a remote image server (<B>dash.ubuntu.com</B>) that maintains a large
+database of albums and musicians.
+
+The requested size for a thumbnail specifies a bounding box (in pixels) of type `QSize` to which
+the thumbnail will be scaled.
+(The aspect ratio of the original image is preserved.) Passing a `QSize(0, `<i>n</i>`)` or `QSize(`<i>n</i>`, 0)`
+defines a square bounding box of <i>n</i> pixels. To obtain an image in its original size, pass `QSize(0, 0)`.
+Sizes with one or both dimensions less than zero return an error.
+
+Original images is never scaled up, so the returned thumbnail may be smaller than its requested size.
+
+All methods are asynchronous and are guaranteed not to block.
+
+The return value is a shared pointer to a \link unity::thumbnailer::qt::Request Request\endlink instance that
+provides access to the scaled thumbnail (or an error message).
 */
+
 class Thumbnailer final
 {
 public:
@@ -114,28 +138,28 @@ public:
     /// @endcond
 
     /**
-     \brief Gets a thumbnail for an album art.
-     \param artist the artist name
-     \param album the album name
-     \param requestedSize the size of the thumbnail we want to obtain.
-     \return A QSharedPointer to a unity::thumbnailer::Request holding the request state.
+    \brief Retrieves a thumbnail for an album cover from the remote image server.
+    \param artist The name of the artist.
+    \param album The name of the album.
+    \param requestedSize The bounding box for the thumbnail.
+    \return A `QSharedPointer` to a unity::thumbnailer::qt::Request holding the request state.
     */
     QSharedPointer<Request> getAlbumArt(QString const& artist, QString const& album, QSize const& requestedSize);
 
     /**
-     \brief Gets a thumbnail for an artist art.
-     \param artist the artist name
-     \param album the album name
-     \param requestedSize the size of the thumbnail we want to obtain.
-     \return A QSharedPointer to a unity::thumbnailer::Request holding the request state.
+    \brief Retrieves a thumbnail for an artist from the remote image server.
+    \param artist The name of the artist.
+    \param album The name of the album.
+    \param requestedSize The bounding box for the thumbnail.
+    \return A `QSharedPointer` to a unity::thumbnailer::qt::Request holding the request state.
     */
     QSharedPointer<Request> getArtistArt(QString const& artist, QString const& album, QSize const& requestedSize);
 
     /**
-     \brief Gets a thumbnail for the given file.
-     \param filePath absolute path to the file to obtain the thumbnail from.
-     \param requestedSize the size of the thumbnail we want to obtain.
-     \return A QSharedPointer to a unity::thumbnailer::Request holding the request state.
+    \brief Extracts a thumbnail from a media file.
+    \param filePath The path to the file to extract the thumbnail from.
+    \param requestedSize The bounding box for the thumbnail.
+    \return A `QSharedPointer` to a unity::thumbnailer::qt::Request holding the request state.
     */
     QSharedPointer<Request> getThumbnail(QString const& filePath, QSize const& requestedSize);
 
