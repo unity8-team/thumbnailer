68794b4b891a5b1904f07047f9e82f17189aada8
Michi Henning <michi.henning@canonical.com>
2015-11-03 22:58:40 +1000

    Removed individual exclusions for tests for PPC now that we are ignoring test failures for PPC globally.

--- tests/stress/stress_test.cpp
+++ tests/stress/stress_test.cpp
@@ -348,11 +348,7 @@ TEST_F(StressTest, mp3)
     add_stats(N_REQUESTS, start, finish);
 }
 
-#if defined(__PPC__)
-TEST_F(StressTest, DISABLED_video)
-#else
 TEST_F(StressTest, video)
-#endif
 {
     if (!supports_decoder("video/x-h264"))
     {
