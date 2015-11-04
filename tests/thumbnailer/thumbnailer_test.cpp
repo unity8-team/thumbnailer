87908b5deb255da05010f8c1107db19f4554cba2
Michi Henning <michi.henning@canonical.com>
2015-11-04 17:58:45 +1000

    Added invalid size error on service side.

--- tests/thumbnailer/thumbnailer_test.cpp
+++ tests/thumbnailer/thumbnailer_test.cpp
@@ -669,6 +669,22 @@ TEST_F(ThumbnailerTest, check_client_access)
     }
 }
 
+TEST_F(ThumbnailerTest, invalid_size)
+{
+    Thumbnailer tn;
+
+    auto request = tn.get_thumbnail(TEST_IMAGE, QSize());
+    try
+    {
+        request->thumbnail();
+        FAIL();
+    }
+    catch (unity::ResourceException const& e)
+    {
+        EXPECT_TRUE(boost::ends_with(e.what(), "invalid size: (-1,-1)")) << e.what();
+    }
+}
+
 TEST_F(ThumbnailerTest, empty_file)
 {
     Thumbnailer tn;
