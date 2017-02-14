d61564eb33548d5234a5edc79f6dae20427fb4c3
Michi Henning <michi.henning@canonical.com>
2017-02-14 17:40:13 +1000

    Remove set_snap_env() again because desktop launch now sets the variable for us.

--- src/env_vars.cpp
+++ src/env_vars.cpp
@@ -38,29 +38,6 @@ namespace thumbnailer
 namespace internal
 {
 
-void EnvVars::set_snap_env()
-{
-#ifdef SNAP_BUILD
-    char const* snap = getenv("SNAP");
-    if (snap)
-    {
-        char const* arch = getenv("SNAP_LAUNCHER_ARCH_TRIPLET");
-        if (!arch || !*arch)
-        {
-            throw runtime_error("Env var SNAP_LAUNCHER_ARCH_TRIPLET not set");
-        }
-
-        string plugin_path = string(snap) + "/usr/lib/" + arch + "/gstreamer-1.0";
-        char const* old_plugin_path = getenv("GST_PLUGIN_PATH");
-        if (old_plugin_path && *old_plugin_path)
-        {
-            plugin_path += string(":") + old_plugin_path;
-        }
-        setenv("GST_PLUGIN_PATH", plugin_path.c_str(), 1);
-    }
-#endif
-}
-
 int EnvVars::get_max_idle()
 {
     char const* c_idle_time = getenv(MAX_IDLE);
